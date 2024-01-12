b899f6439054bff9237008ad6f92f13eb90501b1
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-12 18:38:10 +0600

    variables

--- index.js
+++ index.js
@@ -1,74 +1,66 @@
-// The ternary operator is a concise way to write a simple if-else statement in JavaScript.It takes three operands: a condition to evaluate, a value to return if the condition is true, and a value to return if the condition is false.
+// In JavaScript, variables are used to store data values.They can be declared using the "var", "let", or "const" keywords, and the type of value they can store can be changed during the program execution.
 
-// The syntax for the ternary operator is as follows:
-
-
-// condition ? value_if_true : value_if_false
-
-// Here's an example:
+//     Here's an example of how to declare and use a variable in JavaScript:
 
 
-// let age = 18;
-// let message = age >= 18 ? "You are an adult" : "You are not an adult";
-// console.log(message);
+// Declare a variable named "fname" and assign it the value "John"
+var fname = "John";
 
-// In this example, the condition is age >= 18, which checks if the age variable is greater than or equal to 18. If the condition is true, the value returned is "You are an adult".If the condition is false, the value returned is "You are not an adult".
+// Print the value of the "name" variable to the console
+console.log(fname);
 
-// You can also nest ternary operators to create more complex expressions:
+// Change the value of the "fname" variable to "Jane"
+fname = "Jane";
 
+// Print the updated value of the "fname" variable to the console
+console.log(fname);
+// In this example, we first declare a variable named "name" and assign it the value "John".We then print the value of the "name" variable to the console using the "console.log()" method.
 
-// let num = 5;
-// let message = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
-// console.log(message);
+//     Next, we change the value of the "name" variable to "Jane" and print the updated value to the console again.Since we used the "var" keyword to declare the variable, it can be reassigned to a different value later in the program.
 
+// Variables are a fundamental concept in JavaScript and are used to store all kinds of data, from simple strings and numbers to more complex objects and arrays.
 
-
-
-// const age = 18;
-// const isEligibleToVote = (age >= 18) ? "Yes" : "No";
-// console.log("Is the person eligible to vote?", isEligibleToVote); // output: Is the person eligible to vote? Yes
-
-
-
-// const age = 18;
-// const isCitizen = true;
-// const isEligibleToVote = (age >= 18) ? (isCitizen ? "Yes" : "No, not a citizen") : "No, too young";
-// console.log("Is the person eligible to vote?", isEligibleToVote); // output: Is the person eligible to vote? Yes
+/*
+Ref :
+https://www.w3schools.com/js/js_variables.asp
 
 
-
-// let age = 25;
-// let canDrinkAlcohol = age >= 21 ? "Yes" : "No";
-// console.log(canDrinkAlcohol); // Output: Yes
+*/
 
 
-// Write a ternary operator to check if a given number is even or odd
-function isEvenOrOdd(number) {
-  return number % 2 === 0 ? "even" : "odd";
-}
+// 1. What is the value of x after the following code is executed?
+var x;
+console.log(x); // Output: undefined
+// A. undefined
+// B. null
+// C. 0
+// D. ""
+// Answer: A. undefined
 
-// Test cases
-console.log(isEvenOrOdd(4)); // Output: even
-console.log(isEvenOrOdd(7)); // Output: odd
-console.log(isEvenOrOdd(0)); // Output: even
+// 2. What is the value of y after the following code is executed?
+var y = 5;
+console.log(y); // Output: 5
+// A. undefined
+// B. null
+// C. 0
+// D. 5
+// Answer: D. 5
 
-// Write a ternary operator to check if a user is logged in or not
-let isLoggedIn = true; // Change to false to test
-let message = isLoggedIn ? "Welcome back!" : "Please log in.";
-console.log(message); // Output: Welcome back! (if isLoggedIn is true), or Please log in. (if isLoggedIn is false)
+// 3. What is the value of z after the following code is executed?
+var z = "Hello";
+console.log(z); // Output: "Hello"
+// A. undefined
+// B. null
+// C. 0
+// D. "Hello"
+// Answer: D. "Hello"
 
-
-
-// Declare a variable called "isRaining" and set it to true.
-// Using a ternary operator, assign the value "umbrella" to a variable called "accessory" if it is raining, and "sunglasses" if it is not.
-// Console log the value of the "accessory" variable.
-//     Here's the code snippet:
-
-
-let isRaining = true;
-let accessory = isRaining ? "umbrella" : "sunglasses";
-console.log(accessory);
-
-// When you run this code, it should log "umbrella" to the console, since "isRaining" is set to true.
-
-// You can modify the "isRaining" variable to false and run the code again to see if the value of "accessory" changes to "sunglasses".
+// 4. What is the value of w after the following code is executed?
+var w = 10;
+w = w + 5;
+console.log(w); // Output: 15
+// A. 5
+// B. 10
+// C. 15
+// D. 20
+// Answer: C. 15
