47c476ad25bc958c35bcb2b6c4603397de7a22e3
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-15 00:25:40 +0600

    30.fetch_api

--- index.js
+++ index.js
@@ -1,33 +1,93 @@
-// AJAX(Asynchronous JavaScript and XML) is a technique in JavaScript that allows web pages to be updated asynchronously without requiring a full page refresh.It allows web applications to retrieve and send data to a server in the background without interfering with the display and behavior of the existing page.
+// The Fetch API is a modern JavaScript API for making network requests, such as fetching resources from a server.It is a simpler and more powerful alternative to the older XMLHttpRequest(XHR) API, and it uses promises to handle asynchronous responses.
 
-// Here are the basic steps to use AJAX in JavaScript:
+// Here are the basic steps to use the Fetch API in JavaScript:
 
-// Create an XMLHttpRequest object: The XMLHttpRequest object is used to communicate with the server and retrieve data.
+// Create a fetch request: Use the fetch() function to create a new fetch request.You can pass in the URL of the resource you want to fetch, as well as any options you want to include.
 
-// Open the connection: Use the open() method of the XMLHttpRequest object to specify the HTTP method(such as GET or POST) and the URL of the server.
+// Handle the response: Use the.then() method to handle the response from the server.You can parse the response as JSON, text, or a blob using the.json(), .text(), or.blob() methods, respectively.
 
-// Set up the request: Use the setRequestHeader() method of the XMLHttpRequest object to set any necessary request headers, such as content type.
+// Handle errors: Use the.catch() method to handle any errors that occur during the fetch request.
 
-// Send the request: Use the send() method of the XMLHttpRequest object to send the request to the server.
-
-// Handle the response: Use the onreadystatechange event of the XMLHttpRequest object to handle the response from the server.The response will be available in the responseText or responseXML property of the XMLHttpRequest object.
-
-// Here is an example of how to use AJAX in JavaScript to retrieve data from a server:
+// Here is an example of how to use the Fetch API in JavaScript to fetch data from a server:
 
 
-// const xhr = new XMLHttpRequest();
-// xhr.open("GET", "https://example.com/api/data");
-// xhr.setRequestHeader("Content-Type", "application/json");
-// xhr.send();
-// xhr.onreadystatechange = function () {
-//     if (this.readyState === 4 && this.status === 200) {
-//         const response = JSON.parse(this.responseText);
-//         console.log(response);
-//     }
-// };
+// fetch('https://example.com/api/data')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-// In the example above, we first create an XMLHttpRequest object and set the HTTP method and URL using the open() method.We then set the request header using the setRequestHeader() method and send the request to the server using the send() method.
+// In the example above, we use the fetch() function to make a GET request to the URL 'https://example.com/api/data'.We then use the.json() method to parse the response as JSON, and log the resulting data to the console.If an error occurs during the fetch request, we catch it and log the error to the console.
 
-// We handle the response from the server using the onreadystatechange event and check if the readyState is 4(which means the request is complete) and the status is 200(which means the request was successful).If the request was successful, we parse the response as JSON and log it to the console.
+// The Fetch API can be used to make a variety of network requests, including GET and POST requests, and it supports a wide range of data formats, including JSON, XML, and plain text.It is widely supported by modern browsers and is a powerful tool for building dynamic and responsive web applications.
 
-// AJAX can be used to create dynamic web applications that update the content of the page without requiring a full page refresh.It is commonly used to fetch and display data from APIs, update form data, and create interactive user interfaces.
+
+// Fetch requests can include various options, such as headers, request method(GET, POST, etc.), mode(cors, no - cors, same - origin), and more.These options are passed as an object to the fetch() function as the second argument.
+
+// The Fetch API can be used with any HTTP method, including GET, POST, PUT, DELETE, and more.To use a method other than GET, you need to include the method in the options object when calling fetch().
+
+// The Fetch API can be used to upload files to a server, by creating a new FormData object and appending files to it using the.append() method.
+
+// The Fetch API supports streaming responses, which means that it can process and display data as it is being received from the server, rather than waiting for the entire response to be received before displaying it.
+
+// The Fetch API can be used in both browser and Node.js environments, although some options(such as mode) may not be available in Node.js.
+
+//     Overall, the Fetch API is a powerful and flexible tool for making network requests in JavaScript, and it is widely used in modern web development.
+
+
+
+// Some examples of using the Fetch API in JavaScript to make GET, POST, PUT, and DELETE requests:
+
+// GET request:
+
+
+// fetch('https://example.com/api/data')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// In the example above, we use the fetch() function to make a GET request to the URL 'https://example.com/api/data'.We then use the.json() method to parse the response as JSON, and log the resulting data to the console.If an error occurs during the fetch request, we catch it and log the error to the console.
+
+// POST request:
+
+
+// fetch('https://example.com/api/data', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ foo: 'bar' })
+// })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// In this example, we use the fetch() function to make a POST request to the URL 'https://example.com/api/data'.We include the HTTP method as the method option in the options object, along with the headers and the request body(in this case, a JSON stringified object with a foo property set to 'bar'). We then use the.json() method to parse the response as JSON, and log the resulting data to the console.
+
+// PUT request:
+
+
+// fetch('https://example.com/api/data/123', {
+//     method: 'PUT',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ foo: 'baz' })
+// })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// In this example, we use the fetch() function to make a PUT request to the URL 'https://example.com/api/data/123'.We include the HTTP method as the method option in the options object, along with the headers and the request body(in this case, a JSON stringified object with a foo property set to 'baz'). We then use the.json() method to parse the response as JSON, and log the resulting data to the console.
+
+// DELETE request:
+
+
+// fetch('https://example.com/api/data/123', {
+//     method: 'DELETE'
+// })
+//     .then(response => console.log(response.status))
+//     .catch(error => console.error(error));
+
+// In this example, we use the fetch() function to make a DELETE request to the URL 'https://example.com/api/data/123'.We include the HTTP method as the method option in the options object.We then use the.status property of the response object to log the HTTP status code to the console.
+
+// These examples demonstrate how to use the Fetch API to make various types of HTTP requests in JavaScript.
