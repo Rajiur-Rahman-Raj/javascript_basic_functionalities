47d909b5fcfbdf50912e9191332254af349b9f42
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-14 23:19:40 +0600

    object in js

--- index.js
+++ index.js
@@ -1,134 +1,193 @@
-// A nested for loop is a loop inside another loop.This means that for each iteration of the outer loop, the inner loop will iterate a certain number of times.Nested loops are commonly used when you need to iterate over a two - dimensional array or perform a certain operation for every possible combination of two sets of data.
+// In JavaScript, an object is a data structure that allows you to store collections of data, called properties and methods.An object can represent a real - world entity, such as a person, car, or book, or it can be an abstract concept, such as a color or a shape.
 
-//     Here's an example of a nested for loop in JavaScript:
+// You can create an object in JavaScript using two main syntaxes: object literal notation and constructor notation.
 
-
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j < 3; j++) {
-      console.log(`i = ${i}, j = ${j}`);
-  }
-}
-// In this example, we have an outer loop that iterates five times and an inner loop that iterates three times for each iteration of the outer loop.The inner loop logs the values of both loop variables i and j to the console using a template string.
-
-// When this code is executed, it will output the following:
-/*
-i = 0, j = 0
-i = 0, j = 1
-i = 0, j = 2
-i = 1, j = 0
-i = 1, j = 1
-i = 1, j = 2
-i = 2, j = 0
-i = 2, j = 1
-i = 2, j = 2
-i = 3, j = 0
-i = 3, j = 1
-i = 3, j = 2
-i = 4, j = 0
-i = 4, j = 1
-i = 4, j = 2
-*/
+// Object literal notation
+// Object literal notation is the simplest way to create an object in JavaScript.It involves defining an object as a comma - separated list of name - value pairs enclosed in curly braces { }. Here's an example:
 
 
-// As you can see, the inner loop iterates three times for each iteration of the outer loop, resulting in a total of 15 log statements.
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     sayHello: function () {
+//         console.log('Hello!');
+//     }
+// };
 
-//     That's the basics of nested for loops in JavaScript!
+
+// In this example, we've defined an object called person with four properties: firstName, lastName, age, and sayHello. The sayHello property is a method that logs "Hello!" to the console.
+
+// You can access the properties and methods of an object using dot notation or bracket notation.Here are some examples:
 
 
-// Generating multiplication tables: One practical use of nested for loops is to generate multiplication tables.For example, you can use a nested loop to generate the multiplication table for numbers 1 to 10:
+// console.log(person.firstName); // Output: John
+// console.log(person['lastName']); // Output: Doe
+// person.sayHello(); // Output: Hello!
 
 
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-      console.log(`${i} x ${j} = ${i * j}`);
-  }
-}
-// This code will output the multiplication table for numbers 1 to 10, which can be useful for students learning multiplication.
+// Constructor notation
+// Constructor notation involves defining an object using a constructor function. A constructor function is a special function that is used to create and initialize an object.Here's an example:
 
-// Finding common elements in two arrays: Another practical use of nested for loops is to find common elements between two arrays.You can use two loops to compare each element of one array to every element of another array and find the common ones:
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
+// function Person(firstName, lastName, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.sayHello = function () {
+//         console.log('Hello!');
+//     };
+// }
 
-for (let i = 0; i < array1.length; i++) {
-  for (let j = 0; j < array2.length; j++) {
-      if (array1[i] === array2[j]) {
-          console.log(`${array1[i]} is a common element`);
-      }
-  }
-}
+// let person = new Person('John', 'Doe', 30);
+
+
+// In this example, we've defined a constructor function called Person that takes three arguments: firstName, lastName, and age. The function initializes the object using the this keyword, which refers to the object being created. We've also defined a sayHello method using the same syntax.
+
+// To create a new object using the constructor function, we use the new keyword followed by the name of the constructor function and its arguments.In this case, we've created a new object called person.
+
+// You can access the properties and methods of an object created using constructor notation in the same way as an object created using object literal notation.
 
 
 
-// Drawing shapes
-// One common use of nested for loops is in drawing shapes on a canvas in JavaScript.For example, to draw a checkerboard pattern, you could use the following nested for loop:
+// Object.keys(): Returns an array of the object's own enumerable property names.
 
- 
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-      if ((i + j) % 2 === 0) {
-          // draw black square
-      } else {
-          // draw white square
-      }
-  }
-}
-// This loop iterates over each row and column of the checkerboard and determines whether to draw a black or white square based on the sum of the row and column indices.
+// Object.values(): Returns an array of the object's own enumerable property values.
 
+// Object.entries(): Returns an array of the object's own enumerable property key-value pairs.
 
+// Object.assign(): Copies the values of all enumerable own properties from one or more source objects to a target object, and returns the target object.
+    
+// Object.hasOwnProperty(): Returns a boolean indicating whether the object has the specified property as its own property.
 
-// Finding matches in two arrays
-// Another use of nested for loops is in finding matching elements in two arrays.For example, given two arrays of numbers, you could use the following nested for loop to find pairs of numbers that add up to a specific target value:
+// Object.freeze(): Freezes an object, preventing new properties from being added to it, and existing properties from being removed or changed.
+    
+// Object.seal(): Seals an object, preventing new properties from being added to it, but allowing existing properties to be changed.
 
+// Object.is(): Determines whether two values are the same value.
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [5, 6, 7, 8];
-const target = 10;
+// Object.fromEntries(): Takes an iterable of key - value pairs and returns a new object whose own properties are given by those pairs.
 
-for (let i = 0; i < arr1.length; i++) {
-  for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] + arr2[j] === target) {
-          console.log(`${arr1[i]} + ${arr2[j]} = ${target}`);
-      }
-  }
-}
-// This loop iterates over each element of the first array and each element of the second array and checks if their sum equals the target value.
-
-// Generating permutations
-// A third use of nested for loops is in generating permutations of an array.For example, given an array of letters, you could use the following nested for loop to generate all possible combinations of those letters:
-
-
-const letters = ['a', 'b', 'c'];
-
-for (let i = 0; i < letters.length; i++) {
-  for (let j = 0; j < letters.length; j++) {
-      for (let k = 0; k < letters.length; k++) {
-          console.log(letters[i] + letters[j] + letters[k]);
-      }
-  }
-}
-// This loop iterates over each possible combination of three letters from the original array, generating all possible permutations.
+// Object.getPrototypeOf(): Returns the prototype(i.e., the value of the internal[[Prototype]] property) of the specified object.
 
 
 
-// tips for using nested for loops in JavaScript:
 
-// Keep the loops as simple as possible: It's easy to get lost in complex nested loops, so try to keep them as simple as possible.
 
-// Use meaningful variable names: Choose variable names that clearly indicate what each loop is doing.
 
-// Avoid excessive nesting: Avoid nesting too many loops together as this can make the code hard to read and debug.
+// Object.keys(): This method returns an array of a given object's own property keys. It takes one argument, which is the object whose keys you want to retrieve.
 
-// Keep track of loop counters: Make sure to keep track of loop counters and ensure they're incremented and reset correctly.
+// const myObj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
 
-// Use break statements: Use break statements to exit a loop early if you've found what you're looking for.
+// const keys = Object.keys(myObj);
 
-// Use continue statements: Use continue statements to skip over iterations if certain conditions are met.
+// console.log(keys); // Output: ["name", "age", "city"]
 
-// Be mindful of performance: Nested loops can be computationally expensive, so be mindful of performance and consider alternative approaches if necessary.
 
-// Use the right loop for the job: Consider whether a for, while, or do -while loop is best suited to the task at hand.
+// Object.values(): This method returns an array of a given object's own property values. It takes one argument, which is the object whose values you want to retrieve.
 
-// Use comments: Use comments to explain what each loop is doing and why it's necessary.
+// const myObj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
 
-// Test your code: Finally, make sure to test your code thoroughly, including edge cases and unexpected inputs, to ensure it's working correctly.
+// const values = Object.values(myObj);
+
+// console.log(values); // Output: ["John", 30, "New York"]
+
+
+// Object.entries(): This method returns an array of a given object's own enumerable property key-value pairs. It takes one argument, which is the object whose entries you want to retrieve.
+
+// const myObj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
+
+// const entries = Object.entries(myObj);
+
+// console.log(entries); // Output: [["name", "John"], ["age", 30], ["city", "New York"]]
+
+
+// Object.assign(): This method copies the values of all enumerable properties from one or more source objects to a target object.It takes two or more arguments, where the first argument is the target object and the subsequent arguments are the source objects.
+    
+// const targetObj = { a: 1, b: 2 };
+// const sourceObj1 = { b: 4, c: 5 };
+// const sourceObj2 = { c: 6, d: 7 };
+
+// const newObj = Object.assign(targetObj, sourceObj1, sourceObj2);
+
+// console.log(newObj); // Output: { a: 1, b: 4, c: 6, d: 7 }
+
+
+// Object.hasOwnProperty(): This method returns a boolean indicating whether the object has the specified property as its own property(as opposed to inheriting it from its prototype chain).It takes one argument, which is the name of the property you want to check.
+  
+// const myObj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
+
+// console.log(myObj.hasOwnProperty('name')); // Output: true
+// console.log(myObj.hasOwnProperty('toString')); // Output: false
+
+
+
+// Object.freeze():
+// The Object.freeze() method is used to freeze an object.When an object is frozen, it cannot be modified in any way, including adding or deleting properties, or changing the values of existing properties.
+//     Example:
+
+
+// const obj = {
+//     name: 'John',
+//     age: 30
+// };
+
+// Object.freeze(obj);
+
+// // Trying to modify the object
+// obj.name = 'Peter'; // This will not work
+// obj.city = 'New York'; // This will not work
+
+// console.log(obj); // Output: { name: 'John', age: 30 }
+
+
+// Object.seal():
+// The Object.seal() method is used to seal an object.A sealed object is one where the properties cannot be deleted, but their values can be changed.
+//     Example:
+
+
+// const obj = {
+//     name: 'John',
+//     age: 30
+// };
+
+// Object.seal(obj);
+
+// // Trying to delete a property
+// delete obj.name; // This will not work
+
+// // Trying to add a new property
+// obj.city = 'New York'; // This will not work
+
+// // Trying to modify a property
+// obj.age = 35; // This will work
+
+// console.log(obj); // Output: { name: 'John', age: 35 }
+
+
+
+// Object.is():
+// The Object.is() method is used to compare two values for equality.It returns a boolean value indicating whether the two values are the same or not.
+//     Example:
+
+
+console.log(Object.is('foo', 'foo')); // Output: true
+console.log(Object.is('foo', 'bar')); // Output: false
+console.log(Object.is(null, null)); // Output: true
+console.log(Object.is(0, -0)); // Output: false
