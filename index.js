6230ff563183c6c5aca6e10da0fb9f11fbb82e03
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-15 01:20:58 +0600

    40._template_literals

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-// In JavaScript, a prototype is an object that serves as a template for other objects.Every JavaScript object has a prototype, which is a reference to another object from which it inherits properties and methods.
+// Template literals are a feature introduced in ES6(ECMAScript 2015) that allow us to create strings with embedded expressions, making it easier to concatenate strings and variables together.
 
-// When you create a new object in JavaScript using a constructor function, the object will inherit properties and methods from the prototype of that constructor function. You can add properties and methods to the prototype object, which will then be available to all instances of the object.
-
-// Here is an example of how to use prototypes in JavaScript:
+//     Here's an example of how to use template literals:
 
 
- // define a constructor function
-// function Person(name, age) {
-//     this.name = name;
-//     this.age = age;
-// }
+// const name = "John";
+// const age = 30;
 
-// // add a method to the prototype object
-// Person.prototype.greet = function () {
-//     console.log('Hello, my name is ' + this.name + ' and I am ' + this.age + ' years old.');
-// }
+// const message = `My name is ${name} and I am ${age} years old.`;
 
-// // create an instance of the Person object
-// var person1 = new Person('John', 30);
+// console.log(message); // "My name is John and I am 30 years old."
 
-// // call the greet method on the person1 object
-// person1.greet(); // outputs "Hello, my name is John and I am 30 years old."
-// In the above example, we defined a constructor function Person that takes two parameters name and age.We then added a greet method to the Person.prototype object.Finally, we created an instance of the Person object and called the greet method on it.
+// In this example, we create two variables, name and age, and then use them to create a string using a template literal.We surround the string with backticks() instead of single or double quotes, which allows us to embed expressions inside the string using the ${ } syntax.
 
-// By using prototypes, we can add methods to objects without having to define them in the constructor function, which can lead to cleaner and more efficient code.Additionally, since all instances of an object share the same prototype, changes made to the prototype will be reflected in all instances.
-// In JavaScript, a prototype is an object that serves as a template for other objects.Every JavaScript object has a prototype, which is a reference to another object from which it inherits properties and methods.
+// The expressions inside ${ } are evaluated and their results are concatenated into the final string.In this case, name and age are concatenated into the string message.
 
-// When you create a new object in JavaScript using a constructor function, the object will inherit properties and methods from the prototype of that constructor function. You can add properties and methods to the prototype object, which will then be available to all instances of the object.
-
-// Here is an example of how to use prototypes in JavaScript:
+// Template literals also support multi - line strings, which can be created by simply adding line breaks inside the backticks:
 
 
- // define a constructor function
-// function Person(name, age) {
-//     this.name = name;
-//     this.age = age;
-// }
+// const multiLineMessage = `
+//   My name is ${name}
+//   and I am ${age} years old.
+//   This is a multi-line string.
+// `;
 
-// // add a method to the prototype object
-// Person.prototype.greet = function () {
-//     console.log('Hello, my name is ' + this.name + ' and I am ' + this.age + ' years old.');
-// }
+// console.log(multiLineMessage);
 
-// // create an instance of the Person object
-// var person1 = new Person('John', 30);
+// In this example, we create a multi - line string by adding line breaks inside the backticks.The result is a string with three lines, including the interpolated expressions for name and age.
 
-// // call the greet method on the person1 object
-// person1.greet(); // outputs "Hello, my name is John and I am 30 years old."
-// In the above example, we defined a constructor function Person that takes two parameters name and age.We then added a greet method to the Person.prototype object.Finally, we created an instance of the Person object and called the greet method on it.
-
-// By using prototypes, we can add methods to objects without having to define them in the constructor function, which can lead to cleaner and more efficient code.Additionally, since all instances of an object share the same prototype, changes made to the prototype will be reflected in all instances.
+// Template literals can make our code more readable and easier to maintain, especially when we need to concatenate strings and variables together.They also support advanced features such as tagged templates, which allow us to process template literals with custom functions.
