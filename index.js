ae10aff464af88c2ea6b35d886bb78be1365b915
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-14 23:45:56 +0600

    21.string_methods_in_js

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-// In JavaScript, there are two types of data types: primitive and non - primitive.The main difference between them is how they are stored and accessed in memory.
+// JavaScript provides a number of built -in methods that can be used to manipulate and work with strings.Here are some of the most commonly used string methods in JavaScript:
 
-// Primitive data types are simple and immutable data types that are stored directly in memory.They are called "primitive" because they represent basic values and cannot be broken down any further.There are five primitive data types in JavaScript:
+// charAt(index): Returns the character at the specified index in a string.
 
-// string: A sequence of characters, such as "hello" or "world".
-//number: A numeric value, such as 42 or 3.14.
-//boolean: A logical value that is either true or false.
-//null: A special value that represents "no value" or "nothing".
-//undefined: A special value that represents an undefined value or variable.
+//concat(str1, str2, ...) : Combines two or more strings and returns a new string.
 
-// When a primitive data type is assigned to a variable, the actual value is stored directly in memory at the location of the variable.For example:
+//indexOf(substr, start) : Returns the index of the first occurrence of a specified substring in a string.If the substring is not found, it returns - 1.
+
+// lastIndexOf(substr, start) : Returns the index of the last occurrence of a specified substring in a string.If the substring is not found, it returns - 1.
+
+// slice(start, end) : Extracts a section of a string and returns a new string.The extracted section starts at the specified start index and ends at the specified end index(but does not include the end index).
+
+//substring(start, end) : Similar to the slice() method, but does not allow negative indices.
+
+//replace(regexp, replacement) : Replaces a specified substring or regular expression with another string.
+
+//toLowerCase() : Converts a string to lowercase.
+
+//toUpperCase() : Converts a string to uppercase.
+
+//trim() : Removes whitespace from both ends of a string.
+
+//split(separator, limit) : Splits a string into an array of substrings based on a specified separator.The limit parameter specifies the maximum number of splits to make.
+
+//length : Returns the length of a string.
+
+// Here is an example of how to use some of these string methods in JavaScript:
 
 
-// let x = 42; // x is assigned the value 42, which is a primitive data type
-
-// Non - primitive data types, on the other hand, are more complex data types that are stored as a reference to a location in memory.They are called "non-primitive" because they are made up of one or more primitive data types and can be broken down further.There is only one non - primitive data type in JavaScript, which is:
-
-// object: A collection of properties, where each property has a name and a value.
-
-// When a non - primitive data type is assigned to a variable, the variable stores a reference to the location in memory where the object is stored.For example:
-
-
-// let person = { name: "John", age: 30 }; // person is assigned a reference to an object, which is a non-primitive data type
-
-// In summary, primitive data types are simple and immutable values that are stored directly in memory, while non - primitive data types are more complex and are stored as a reference to a location in memory.Understanding the difference between them is important when working with data in JavaScript, as it affects how data is stored and accessed in memory.
+// let str = "Hello, World!";
+// console.log(str.charAt(1)); // e
+// console.log(str.concat(" How are you?")); // Hello, World! How are you?
+// console.log(str.indexOf("World")); // 7
+// console.log(str.slice(0, 5)); // Hello
+// console.log(str.replace("World", "Universe")); // Hello, Universe!
+// console.log(str.toLowerCase()); // hello, world!
+// console.log(str.toUpperCase()); // HELLO, WORLD!
+// console.log(str.trim()); // Hello, World!
+// console.log(str.split(", ")); // ["Hello", "World!"]
+// console.log(str.length); // 13
+// By using these string methods, you can easily manipulate and work with strings in JavaScript.
