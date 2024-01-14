4f38a628cf83b5bfd3f3410dce6b5ec36d043ca9
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-14 23:24:06 +0600

    arrow_functions

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-// In JavaScript, a function is a block of reusable code that performs a specific task.Functions are a fundamental building block of programming and are used to organize and modularize code.
+// Arrow functions are a shorthand syntax for creating functions in JavaScript.They were introduced in ES6(ECMAScript 2015) and are a popular way of writing functions because they are more concise and easier to read.
 
-//     Here's an example of a simple function:
+//     Here's an example of an arrow function that takes one parameter and returns a greeting message:
 
 
-// function greet(name) {
-//     console.log("Hello, " + name + "!");
+// const greet = (name) => {
+//     return "Hello, " + name + "!";
 // }
-// This function is called greet and it takes one parameter called name.When the function is called, it will log a message to the console that says "Hello, " followed by the value of the name parameter.
 
-// To call the greet function, you simply need to provide a value for the name parameter:
+// This function is called greet and it takes one parameter called name.The arrow function syntax consists of the => symbol, which separates the function parameters from the function body. In this example, the function body simply returns a string that says "Hello, " followed by the value of the name parameter.
 
-
-// greet("John"); // logs "Hello, John!"
-// Functions can also return values.Here's an example of a function that takes two parameters and returns their sum:
+// To call the greet function, you can use it like this:
 
 
-// function add(a, b) {
-//     return a + b;
-// }
-// This function is called add and it takes two parameters called a and b.When the function is called, it will return the sum of a and b.
+// console.log(greet("John")); // logs "Hello, John!"
+// In this example, the greet function is called with the value "John" as its parameter.The function returns the greeting message "Hello, John!", which is then logged to the console.
 
-// To use the add function, you can call it like this:
+// Arrow functions are especially useful when you need to write short, concise functions.Here's an example of an arrow function that takes no parameters and returns a random number between 1 and 10:
 
 
-// var result = add(2, 3); // result is now 5
-// In this example, the add function is called with the values 2 and 3 as its parameters.The function returns the sum of those two values, which is 5. The result is then assigned to a variable called result.
+// const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
+// This function is called getRandomNumber and it uses the Math.random() method to generate a random number between 0 and 1. It then multiplies that number by 10 and rounds it down to the nearest integer using the Math.floor() method.Finally, it adds 1 to the result to get a random number between 1 and 10.
+
+// To call the getRandomNumber function, you can use it like this:
+
+
+// console.log(getRandomNumber()); // logs a random number between 1 and 10
+// I hope this gives you a basic understanding of arrow functions in JavaScript! Let me know if you have any further questions.
