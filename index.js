2258f5e3afe3d1da4e798ee5773e453fa810f377
Rajiur-Rahman-Raj <rajiurrahmanraj2021@gmail.com>
2024-01-19 20:15:50 +0600

    asynchronous function in js

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-// Modules are a way of organizing and encapsulating code in JavaScript.They allow you to write reusable code that can be easily shared between different parts of your application.
+// Asynchronous programming is a way of writing code that allows multiple tasks to be executed simultaneously without blocking the execution of other tasks.In JavaScript, asynchronous programming is typically done using callbacks, promises, or async / await.
 
-// Before the introduction of ES6, JavaScript didn't have a built-in way to implement modules. Developers had to use various workarounds, such as the CommonJS and AMD specifications, or create their own custom module systems.
+// Callbacks:
+// Callbacks are functions that are passed as arguments to other functions and are called when an asynchronous operation is complete.For example, the setTimeout() function takes a callback function as an argument, which is called after a certain amount of time has passed.
 
-// With the introduction of ES6, JavaScript now has a built -in module system.Modules in ES6 are declared using the import and export statements.
+ 
+// setTimeout(() => {
+//     console.log('Hello, world!');
+// }, 1000);
+// Promises:
+// Promises are objects that represent a value that may not be available yet, but will be resolved in the future.Promises have two states: pending and fulfilled or rejected.Promises can be chained together to create a sequence of asynchronous operations.
 
-//     Here's an example of how to define and use a module in JavaScript:
-
-
- // math.js
-// export const add = (a, b) => {
-//     return a + b;
+   
+// const fetchData = () => {
+//     return new Promise((resolve, reject) => {
+//         fetch('https://jsonplaceholder.typicode.com/users')
+//             .then(response => {
+//                 if (response.ok) {
+//                     resolve(response.json());
+//                 } else {
+//                     reject(new Error('Failed to fetch data'));
+//                 }
+//             })
+//             .catch(error => {
+//                 reject(error);
+//             });
+//     });
 // };
 
- // main.js
-// import { add } from './math.js';
+// fetchData()
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// Async / await:
+// Async / await is a newer syntax for asynchronous programming in JavaScript that makes code easier to read and write.Async functions return a promise, and the await keyword is used to wait for a promise to be resolved before continuing execution.
 
-// console.log(add(2, 3)); // Outputs 5
-
-// In this example, we define a module math.js that exports a function add. We then import the add function from math.js into another module main.js using the import statement.We can then use the add function in main.js as if it were defined in that module.
-
-// There are different ways to export and import modules in JavaScript, such as default exports and named exports.Here are some examples:
-
-
- // math.js (default export)
-// const add = (a, b) => {
-//     return a + b;
+  
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         if (response.ok) {
+//             const data = await response.json();
+//             console.log(data);
+//         } else {
+//             throw new Error('Failed to fetch data');
+//         }
+//     } catch (error) {
+//         console.error(error);
+//     }
 // };
 
-// export default add;
-
- // main.js (default import)
-// import add from './math.js';
-
-// console.log(add(2, 3)); // Outputs 5
-
- // math.js (named export)
-// export const add = (a, b) => {
-//     return a + b;
-// };
-
-// export const subtract = (a, b) => {
-//     return a - b;
-// };
-
- // main.js (named import)
-// import { add, subtract } from './math.js';
-
-// console.log(add(2, 3)); // Outputs 5
-// console.log(subtract(3, 2)); // Outputs 1
-
-// In summary, modules in JavaScript allow you to organize and encapsulate code, and make it easy to reuse and share between different parts of your application.The import and export statements provide a simple and standardized way to define and use modules in your JavaScript code.
+// fetchData();
+// In summary, asynchronous programming in JavaScript allows for non - blocking execution of tasks, and can be achieved using callbacks, promises, or async / await.Each method has its own strengths and weaknesses, and the choice of which to use depends on the specific use case.
